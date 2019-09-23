ddb381793af3b6d165d8a39854a8c04c96bc6326
Vanna Winland <vgwinland@gmail.com>
2019-09-23 20:58:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,9 @@
 class Shoe 
-  attr_accessor :brand, :color, :size, :material
-  attr_reader :condition
+  attr_accessor :condition, :color, :size, :material
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
 end
