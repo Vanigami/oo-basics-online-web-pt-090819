b7cd86b665a7c7ac4af5ac0c38a5d96bf3c56d0e
Vanna Winland <vgwinland@gmail.com>
2019-09-23 20:53:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,12 @@ class Book
   
   def initialize(title)
     @title = title 
+  end 
+  
+  def title 
+    @title 
   end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
